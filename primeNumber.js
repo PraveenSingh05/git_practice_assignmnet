ce2098556e572fd007440d7c2774bbbc494e84d2
PraveenSingh05 <05praveensingh.ps@gmail.com>
2024-01-09 15:04:32 +0530

    checkprime done

---
+++ primeNumber.js
@@ -0,0 +1,12 @@
+
+function checkPrime(num){
+    // intetionally edge case is not handled like if we pass 1 it will throw true but it is false;
+    for(let i=2; i<num; i++){
+        if(num%i===0){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(checkPrime(1));
