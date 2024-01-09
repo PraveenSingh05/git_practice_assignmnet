f5e2b828e3179aecf492512d7a196fe77786e569
PraveenSingh05 <05praveensingh.ps@gmail.com>
2024-01-09 18:01:03 +0530

    prime Number bug fixed

--- primeNumber.js
+++ primeNumber.js
@@ -1,6 +1,9 @@
 
 function checkPrime(num){
     // intetionally edge case is not handled like if we pass 1 it will throw true but it is false;
+    if(num===1){
+        return false;
+    }
     for(let i=2; i<num; i++){
         if(num%i===0){
             return false;
